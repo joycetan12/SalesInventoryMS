8711df317350abedff386000ab9bea941bf40ae3
Joyce Tan <joycetan1298@gmail.com>
2023-03-13 17:24:27 -0400

    updated queries

--- sims/src/main/java/com/perscholas/sims/repository/SaleRepository.java
+++ sims/src/main/java/com/perscholas/sims/repository/SaleRepository.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import com.perscholas.sims.dto.ProfitByMonth;
 import com.perscholas.sims.dto.RevenueByMonth;
 import com.perscholas.sims.dto.SalesByMonth;
+import com.perscholas.sims.dto.TopSaleItems;
 import com.perscholas.sims.model.Sale;
 
 @Repository
@@ -26,37 +27,37 @@ public interface SaleRepository extends JpaRepository<Sale, Long> {
 	List<Sale> findByOrderByDateOfSaleDesc();
 	
 	@Query(
-		value = "SELECT SUM(sale_price*quantity) FROM sale",
+		value = "SELECT SUM(sale_price*quantity) FROM sale WHERE paid = 1",
 		nativeQuery = true)
 	BigDecimal getTotalRevenue();
 	
 	@Query(
-		value = "SELECT (SUM(s.quantity*s.sale_price) - SUM(s.quantity*i.cost)) FROM sale s, item i WHERE s.item_id = i.id",
+		value = "SELECT (SUM(s.quantity*s.sale_price) - SUM(s.quantity*i.cost)) FROM sale s, item i WHERE s.item_id = i.id AND s.paid = 1",
 		nativeQuery = true)
 	BigDecimal getTotalProfit();
 	
 	@Query(
-		value = "SELECT SUM(quantity) FROM sale",
+		value = "SELECT SUM(quantity) FROM sale WHERE paid = 1",
 		nativeQuery = true)
 	int getTotalSales();
 
 	@Query(
-		value = "SELECT item_id, SUM(quantity), SUM(sale_price*quantity) FROM sale GROUP BY item_id ORDER BY SUM(quantity) DESC LIMIT 3",
+		value = "SELECT i.name as 'itemName', SUM(s.quantity) as 'quantity', SUM(s.sale_price*s.quantity) as 'revenue' FROM sale s, item i WHERE s.item_id = i.id AND paid = 1 GROUP BY i.name ORDER BY SUM(s.quantity) DESC LIMIT 3",
 		nativeQuery = true)
-	List<Object[]> findTop3ByTotalQuantity();
+	List<TopSaleItems> findTop3ByTotalQuantity();
 	
 	@Query(
-		value = "SELECT MONTH(date_of_sale) as 'month', SUM(quantity) as 'salesCount' FROM sale GROUP BY MONTH(date_of_sale) ORDER BY MONTH(date_of_sale)",
+		value = "SELECT MONTH(date_of_sale) as 'month', SUM(quantity) as 'salesCount' FROM sale WHERE paid = 1 GROUP BY MONTH(date_of_sale) ORDER BY MONTH(date_of_sale)",
 		nativeQuery = true)
 	List<SalesByMonth> getSalesCountByMonth();
 	
 	@Query(
-		value = "SELECT MONTH(date_of_sale) as 'month', (SUM(s.quantity*s.sale_price) - SUM(s.quantity*i.cost)) as 'profit' FROM sale s, item i WHERE s.item_id = i.id GROUP BY MONTH(date_of_sale) ORDER BY MONTH(date_of_sale)",
+		value = "SELECT MONTH(date_of_sale) as 'month', (SUM(s.quantity*s.sale_price) - SUM(s.quantity*i.cost)) as 'profit' FROM sale s, item i WHERE s.item_id = i.id AND s.paid = 1 GROUP BY MONTH(date_of_sale) ORDER BY MONTH(date_of_sale)",
 		nativeQuery = true)
 	List<ProfitByMonth> getProfitByMonth();
 	
 	@Query(
-		value = "SELECT MONTH(date_of_sale) as 'month', SUM(s.quantity*s.sale_price) as 'revenue' FROM sale s, item i WHERE s.item_id = i.id GROUP BY MONTH(date_of_sale) ORDER BY MONTH(date_of_sale)",
+		value = "SELECT MONTH(date_of_sale) as 'month', SUM(s.quantity*s.sale_price) as 'revenue' FROM sale s, item i WHERE s.item_id = i.id AND s.paid = 1 GROUP BY MONTH(date_of_sale) ORDER BY MONTH(date_of_sale)",
 		nativeQuery = true)
 	List<RevenueByMonth> getRevenueByMonth();
 	
